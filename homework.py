506c2f5e357b5c803372616ecc8f20830c0d52a5
Иван Озеров <vanzup@mail.ru>
2022-07-21 20:36:32 +0300

    change homework.py

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import List
 
 
@@ -12,13 +12,16 @@ class InfoMessage:
     speed: float
     calories: float
 
-    def get_message(self):
+    TEXT_MESSAGE: str = (
+        'Тип тренировки: {training_type}; '
+        'Длительность: {duration:.3f} ч.; '
+        'Дистанция: {distance:.3f} км; '
+        'Ср. скорость: {speed:.3f} км/ч; '
+        'Потрачено ккал: {calories:.3f}.')
+
+    def get_message(self) -> str:
         """Возвращает строку сообщения о тренировке."""
-        return(f'Тип тренировки: {self.training_type}; '
-               f'Длительность: {self.duration:.3f} ч.; '
-               f'Дистанция: {self.distance:.3f} км; '
-               f'Ср. скорость: {self.speed:.3f} км/ч; '
-               f'Потрачено ккал: {self.calories:.3f}.')
+        return(self.TEXT_MESSAGE.format(**asdict(self)))
 
 
 class Training:
